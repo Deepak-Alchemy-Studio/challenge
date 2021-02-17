40d4443249b3a8ee02124eacf406b62170a2da6b
Deepak-Alchemy-Studio <79159597+Deepak-Alchemy-Studio@users.noreply.github.com>
2021-02-17 14:45:18 +0530

    OneMob Challenge task

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,30 +1,41 @@
 import "./Form.css";
 import $ from "jquery";
 
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 
 const Form = () => {
   const inputRef = useRef();
+  const [error, setError] = useState(false);
+
   const triggerUploader = () => {
     inputRef.current.click();
   };
 
-  const changeInput = async () => {
+  const changeInput = async (e) => {
     const images = $(".images");
-    const reader = new FileReader();
-    reader.onload = (e) => {
-      images.append(
-        '<div class="img" onclick="this.remove()" style="background-image: url(\'' +
-          e.target.result +
-          '\');" rel="' +
-          e.target.result +
-          '"><span>remove</span></div>'
-      );
-    };
-    console.log(inputRef.current.files[0]);
-    if (inputRef.current.files[0]) {
-      await alert("file uploaded");
-      reader.readAsDataURL(inputRef.current.files[0]);
+    const selected = e.target.files[0];
+    const allowedTypes = ["image/png", "image/jpg", "image/jpeg"];
+    if (selected && allowedTypes.includes(selected.type)) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        e.preventDefault();
+        console.log(e.target);
+        images.append(
+          '<div class="img" onclick="this.remove()" style="background-image: url(\'' +
+            e.target.result +
+            '\');" rel="' +
+            e.target.result +
+            '"><span>remove</span></div>'
+        );
+      };
+      console.log(inputRef.current.files[0]);
+      if (inputRef.current.files[0]) {
+        alert("file uploaded");
+        reader.readAsDataURL(inputRef.current.files[0]);
+      }
+    } else {
+      setError(true);
+      alert("Upload Error");
     }
   };
 
@@ -32,7 +43,7 @@ const Form = () => {
     <div className="form">
       <div className="wrapper">
         <div className="sections">
-          <h1 className="header">File uploader</h1>
+          <h1 className="header">Image uploader</h1>
           <div className="images">
             <div className="pic" onClick={() => triggerUploader()}>
               add
